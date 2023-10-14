e6ee78f6a1999be0199e921a91b54204722c436b
ansnekit <ansnekit@gmial.com>
2023-10-14 19:59:33 +0300

    init project

---
+++ layouts/LayoutMain.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id="LayoutMain" class="w-full fixed z-10"></div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <LayoutMain>
+        <div id="IndexPage" class="mt-4 mx-auto px-2 max-w-[1200px] bg-green-500">
+            <h1>Hello</h1>
+        </div>
+    </LayoutMain>
+</template>
+
+<script setup lang="ts">
+import LayoutMain from '@/layouts/LayoutMain.vue';
+
+
+</script>
