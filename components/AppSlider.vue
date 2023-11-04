7f75ebb79e6d6f2a5c2807f5d74c6931a2b9464b
ansnekit <ansnekit@gmial.com>
2023-11-04 20:27:06 +0300

    fix banneer

---
+++ components/AppSlider.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full bg-[#f0f4f7]">
+        <div class="">
+            
+        </div>
+        <ul>
+            <li>
+                <RouterLink to="/"></RouterLink>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
